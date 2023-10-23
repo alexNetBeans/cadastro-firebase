d19d3c70d312ae122012c18125db138568fb3fde
Alex Sandro <alexnetbeans@gmail.com>
2023-10-23 18:23:00 -0300

    inclusão de usuário

---
+++ js/crud/firebase-init.js
@@ -0,0 +1,15 @@
+
+const firebaseConfig = 
+        {
+
+            apiKey: "AIzaSyBLm5uJUDS8U_oTvZmCA3nopy-uSSkCtYI",
+            authDomain: "gastosdb-459ff.firebaseapp.com",
+            databaseURL: "https://gastosdb-459ff-default-rtdb.europe-west1.firebasedatabase.app",
+            projectId: "gastosdb-459ff",
+            storageBucket: "gastosdb-459ff.appspot.com",
+            messagingSenderId: "580106456265",
+            appId: "1:580106456265:web:e2168bea62d0fb9945d118"
+            
+        };
+
+        firebase.initializeApp(firebaseConfig);
---
+++ js/crud/insert.js
@@ -0,0 +1,63 @@
+
+// Cadastra dados no firebase
+
+function insertData( data )
+{
+    
+    firebase.database().ref('gastos-db').child('cliente')
+    .push( data )
+    .then ((s)=>
+    {
+        window.location = 'cadConcluido.html';
+
+    }, (error) => {
+        if (error) {
+            showMsg('erro');
+        } else {
+        // Data saved successfully!
+        showMsg('success');
+        }
+    }).key   
+}
+
+// Pega dados do formulário.
+
+function getDataForm(){
+
+    let avatar = $('#imgPlace').attr('src');
+
+    let nome   = $('#nome').val();
+    let sexo   = $('#sexo').val();
+    let email  = $('#email').val();
+    let senha  = $('#senha').val();
+
+    let data = 
+    {
+       'nome': nome,
+       'sexo': sexo,
+       'email': email,
+       'senha': senha,
+       'avatar': avatar
+    }
+
+    insertData( data ); 
+};
+
+// Botão Cadastrar.
+
+$('#btnCad').on('click', ((data)=>{
+
+    getDataForm();
+
+}));
+
+/*
+	const dbRef = firebase.database().ref('gastos-db');
+	dbRef.child("cliente").get().then((snapshot) => 
+	{
+		snapshot.forEach((data)=>{
+        console.log(data.key)
+    });
+	});
+
+*/
