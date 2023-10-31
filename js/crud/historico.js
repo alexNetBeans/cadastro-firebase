8c55859af331e7e5eee7291c73dd3f2989fadd74
Alex Sandro <alexnetbeans@gmail.com>
2023-10-31 19:18:24 -0300

    Consulta de histórico funcionado

--- js/crud/historico.js
+++ js/crud/historico.js
@@ -1,26 +1,105 @@
 
-function getHitoric ( idUser, mes ){
 
-    /*
-    firebase.database().ref('gastos-db').child('cliente/-NhhMVcCdWyVH3ylyTBQ/historico/setembro')
-    .push( {descrição: 'Ração dos Cães', valor: 60, detalhes: 'sem detalhes'}).key
-*/
+// Listagem histórico do mês selecionado. / Por dia do mês.
+
+function getHistoricbyDay ( idUser, ano, mes ){
+
+    $('#backIcon').remove();
+    $('#tabela').empty();
+
     const dbRef = firebase.database().ref('gastos-db');
-	dbRef.child("cliente/" + idUser + "/historico/" + mes ).get().then(( snapshot )=> 
+	dbRef.child("cliente/" + idUser + "/historico/" + ano + '/' + mes ).get().then(( snapshot )=> 
 	{
-            snapshot.forEach((d)=>{
+            let col = '<tr><td> Data <td> Descrição <td> Valor <td class="text-center"> Detalhes';
+            $('#tabela').append( '<tbody><tr>' );
+            $('#tabela').append( col );
+            
+            let msgTbl = $('#msgTbl');
+            msgTbl.text( ' Relação dos gastos do mês de ' + mes + ' de ' + ano  );
+            
+            snapshot.forEach(( data )=>{
                 
-                console.log( d.val().descrição );
-                console.log( d.val().detalhes );
-                console.log( d.val().valor );
+                let line = '<tr><td>' + data.val().data +'<td>' + data.val().descrição + '<td>' + data.val().valor + '<td> <i class="bi bi-eye-fill"></i>';
+                $('#tabela').append( line );
                 
+                console.log( data );
+            });
+    });    
+        
+    // Link de voltar para tabela de anos.
+    
+        let linkBack = $('<div id="backIcon">');
+        
+        $('#tabela').after( linkBack );
+        $('#backIcon').attr('onClick','getHistoricFromMonth ( getidUser(),' + ano + ')' );
+        $('#backIcon').addClass(' bi bi-box-arrow-in-left fs-4 ');
+        
+}
+
+// Listagem histórico por ano.
+
+function getHistoricFull ( idUser ){
+
+    let msgTbl = $('#msgTbl');
+    msgTbl.text( ' Relação por anos' );
+
+    $('#backIcon').remove();
+    $('#tabela').empty();
+    $('#tabela').append( '<tbody><tr>' );
+        
+    const dbRef = firebase.database().ref('gastos-db');
+	dbRef.child("cliente/" + idUser + "/historico/").get().then(( snapshot )=> 
+	{
+            // console.log( snapshot.val() );
+        
+            snapshot.forEach(( data )=>{
+                
+                $('#tabela').append( '<tr><td onClick="getHistoricFromMonth ( getidUser(), this.innerText )">' + data.key );
+
             });
     });    
 }
 
+// Listagem histórico por mês
+
+function getHistoricFromMonth ( idUser, ano ){
+
+    $('#backIcon').remove();
+    $('#tabela').empty();
+    
+    const dbRef = firebase.database().ref('gastos-db');
+	dbRef.child("cliente/" + idUser + "/historico/" + '/' + ano ).get().then(( snapshot )=> 
+	{
+            $('#tabela').empty();
+            $('#tabela').append( '<tbody><tr>' );
+            let msgTbl = $('#msgTbl');
+            
+            msgTbl.text( ' Relação por meses do ano de ' + ano );    
+           
+            snapshot.forEach(( data )=>{
+            
+                $('#tabela').append( '<tr><td onClick="getHistoricbyDay ( getidUser(),' + ano + ', this.innerText )">' + data.key );
+                
+            });
+    });    
+
+    // Link de voltar para tabela de anos.
+    
+    let linkBack = $('<div id="backIcon">');
+        
+    $('#tabela').after( linkBack );
+    $('#backIcon').attr('onClick','getHistoricFull ( getidUser() )' );
+    $('#backIcon').addClass(' bi bi-box-arrow-in-left fs-4 ');
+
+}
+
+
+// link visualizar histórico.
+
 $('#historico').on('click', ((data)=>{
 
     let id = sessionStorage.getItem("id");
-    getHitoric( idUser, mes );
+
+    getHistoricFull ( getidUser() );
 
 }));
--- js/session.js
+++ js/session.js
@@ -6,8 +6,42 @@ function getidUser(){
     return idUser;
 }
 
+function getNameUser(){
+    
+    let nameUser = sessionStorage.getItem("nome");
+    return nameUser;
+}
+
+function getAvatarUser(){
+    
+    let avatarUser = sessionStorage.getItem("avatar");
+    return avatarUser;
+}
+
+function getemailUser(){
+    
+    let emailUser = sessionStorage.getItem("email");
+    return emailUser;
+}
 
 
+//  Recupera dados do usuário logado.
+
+function getDataSessionUser(){
+
+    $('#FullName').text( getNameUser() );
+    $('#name').text( getNameUser() );
+    $('#email').text( getemailUser() );
+    $('#avatar').attr('src', getAvatarUser());
+    $('#avatar').attr('width','80');
+
+    $('#FullName').addClass('fs-2 text-center');
+    $('#FullName').css('vertical-align', 'middle');
+    
+
+
+    // $('#senha')
+}
 
 
 // Finaliza Sessão
